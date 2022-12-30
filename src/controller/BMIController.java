91e638edbbe2da7a31a0b07f434a2508f04537b7
GoEun <w2219@e-mirim.hs.kr>
2022-12-30 10:07:40 +0900

    init commit

---
+++ src/controller/BMIController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import view.BMIView;
+
+public class BMIController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new BMIView();
+
+	}
+
+}
